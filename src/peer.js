21565c17d84c64f5bf1f49155ac0db4207bb868f
swapan <swapan@gmail.com>
2023-06-16 15:59:04 +0530

    removed something

--- src/peer.js
+++ src/peer.js
@@ -1,9 +1,9 @@
-import Peer from "peerjs";
+// import Peer from "peerjs";
 
-const myPeer = new Peer(undefined, {
-  host: "/",
-  // port: 3001,
-  // path: "/peerjs",
-});
+// const myPeer = new Peer(undefined, {
+//   host: "/",
+//   // port: 3001,
+//   // path: "/peerjs",
+// });
 
-export default myPeer;
+// export default myPeer;
